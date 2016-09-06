b8eafc3b2278fd0a9633237a99628805d7f296cd
THACHER <thachert1d@gmail.com>
2016-09-05 22:05:03 -0700

    heroku config updates

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/pottertest'
+  },
+  production: {
+  client: 'pg',
+  connection: 'postgres://jrpiuqyvuopsqb:dMjNlEWP7UmeVWoD_amzhodvPw@ec2-54-235-183-28.compute-1.amazonaws.com:5432/dfb4gnjipfnvnb'
   }
 };
