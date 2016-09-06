adfbe19f87f05026e265c5e246b7ec08e541fd23
THACHER <thachert1d@gmail.com>
2016-09-05 19:04:43 -0700

    data on the page

--- routes/characters.js
+++ routes/characters.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-var knex = require('../db/knex');
-
+var knex = require('knex')({
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  });
+  
 function characters() {
   return knex('characters');
 }
 
-function hogwarts_houses () {
-  return knex('hogwarts_houses');
-}
 
-/* GET users listing. */
-router.get('/characters', function(req, res, next) {
-  characters().select().then(function(results) {
+router.get('/characters', function(req, res) {
+  characters().select().then(function(results){
+    res.render('characters/index', {characters: results});
   });
-  res.render('characters/index', {characters: results});
 });
-
 
 module.exports = router;
