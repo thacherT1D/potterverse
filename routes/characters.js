98d733bbe178165810f1c780659579c2d6fad229
THACHER <thachert1d@gmail.com>
2016-09-05 18:50:10 -0700

    got some shit working, broke some other shit

--- routes/characters.js
+++ routes/characters.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var knex = require('../db/knex');
+
+function characters() {
+  return knex('characters');
+}
+
+function hogwarts_houses () {
+  return knex('hogwarts_houses');
+}
 
 /* GET users listing. */
 router.get('/characters', function(req, res, next) {
-  res.render('characters/index');
+  characters().select().then(function(results) {
+  });
+  res.render('characters/index', {characters: results});
 });
 
 
