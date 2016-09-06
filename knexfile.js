e9bc8a4363f7210e003f3dc8737870bb82ffb14e
THACHER <thachert1d@gmail.com>
2016-09-05 19:40:22 -0700

    working and displaying basic character data

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,5 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/pottertest'
-  },
-
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL
   }
 };
--- routes/characters.js
+++ routes/characters.js
@@ -1,20 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var knex = require('knex')({
-    client: 'pg',
-    connection: process.env.DATABASE_URL
-  });
-  
+var knex = require('../db/knex');
+
 function characters() {
   return knex('characters');
 }
 
-
 router.get('/characters', function(req, res) {
   characters().select().then(function(results){
-    res.render('characters/index', {characters: results});
+  res.render('characters/index', {characters: results});
   });
+  // res.render('characters/index');
+
 });
+
+
+
 
 module.exports = router;
