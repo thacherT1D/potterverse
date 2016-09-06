fc7b0f0ce1be3c7179a57952c181b79db81f4473
THACHER <thachert1d@gmail.com>
2016-09-05 19:44:12 -0700

    baisc character menu api endpoint working

--- routes/characters.js
+++ routes/characters.js
@@ -11,11 +11,15 @@ router.get('/characters', function(req, res) {
   characters().select().then(function(results){
   res.render('characters/index', {characters: results});
   });
-  // res.render('characters/index');
-
 });
 
-
+router.get('/characters/menu', (req, res) => {
+  characters().select().then(function(results){
+    var characterResults = {characters: results};
+    var jsonCharacterResults = JSON.stringify(characterResults);
+    res.send(jsonCharacterResults);
+  });
+});
 
 
 module.exports = router;
