dd9965b9c8f96aeb207947ceebb2744349d23b51
THACHER <thachert1d@gmail.com>
2016-09-06 22:49:46 -0700

    heroku

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,6 @@ module.exports = {
   },
   production: {
   client: 'pg',
-  connection: 'postgres://jrpiuqyvuopsqb:dMjNlEWP7UmeVWoD_amzhodvPw@ec2-54-235-183-28.compute-1.amazonaws.com:5432/dfb4gnjipfnvnb'
+  connection: 'postgres://xijbjzhbktmnpm:xC1g1zDxDHQv1S5UDTefxLJNY1@ec2-54-243-208-195.compute-1.amazonaws.com:5432/d77tdg23s5bel7'
   }
 };
