410ea964ac585ea3c3f27f7e35fc3d72ddd7bcf8
THACHER <thachert1d@gmail.com>
2016-10-14 13:32:28 -0700

    psql, seed, migrations updates

--- app.js
+++ app.js
@@ -1,68 +1,81 @@
-require('dotenv').load({silent: true});
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config;
+}
+
 
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = express();
 var handlebars = require('handlebars');
+// var methodOverride = require('method-override');
+
+app.disable('x-powered-by');
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(methodOverride('_method'));
 
 var routes = require('./routes/index');
 var characters = require('./routes/characters');
 var data = require('./routes/data');
 
-var methodOverride = require('method-override');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
-
 app.use('/', routes);
 app.use('/', characters);
 app.use('/', data);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((_req, res) => {
+  res.sendStatus(404);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+  // console.error(JSON.stringify(err, null, 2));
+  // if(err.status) {
+  //   return res
+  //     .status(err.status)
+  //     .set('Content-Type', 'text/plain')
+  //     .send(err.statusText)
+  // }
+  console.error(err.stack);
+  res.sendStatus(500);
 });
 
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  if (app.get('env') !== 'test') {
+    console.log('Listening on port', port);
+  }
+});
 
 module.exports = app;
