5a60d48296ce6839b92e4a7a86b827d1e1758040
THACHER <thachert1d@gmail.com>
2016-10-14 15:58:33 -0700

    removed handlebars.js

--- js/handlebars.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-  Handlebars.registerHelper('select', function( value, options ){
-    var $el = $('<select />').html( options.fn(this) );
-    $el.find('[value="' + value + '"]').attr({'selected':'selected'});
-    return $el.html();
-  });
-
-});
